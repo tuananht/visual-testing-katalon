a3b1088cf7e4d535cdae658316d90f8212ecf877
tuananht <43736150+tuananht@users.noreply.github.com>
2022-09-27 11:00:00 +0700

    API Test: add new test suite/test suite collection

---
+++ Test Suites/API_Katalon Pricing.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>API_Katalon Pricing</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>ceb37cd9-bb02-49fb-8c5e-238d3c35a588</testSuiteGuid>
+   <testCaseLink>
+      <guid>30fd30ba-b14d-43e9-bc60-4458d7e64766</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API_Katalon Pricing</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
