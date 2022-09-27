0109e76f00909f79cbc87405de6120494cd4d253
tuananht <43736150+tuananht@users.noreply.github.com>
2022-09-27 10:43:37 +0700

    Visual Testing: add new test suite/test suite collection

---
+++ Test Suites/VS_TS_Katalon Pricing.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>VS_TS_Katalon Pricing</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>f7046b33-d20c-4e17-87f4-2125032302d2</testSuiteGuid>
+   <testCaseLink>
+      <guid>1a7b7871-9ebc-494b-bc2c-dfe14c7d7825</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/VS_Katalon Pricing</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
